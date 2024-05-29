e63f317ba952aef6821d9e78efffe25cf8397d11
Uneees <belabbes1021@gmail.com>
2024-05-29 11:41:52 +0200

    version Younes

---
+++ app/src/main/java/com/example/devicetrack/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.devicetrack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.devicetrack.databinding.ActivityMainBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
