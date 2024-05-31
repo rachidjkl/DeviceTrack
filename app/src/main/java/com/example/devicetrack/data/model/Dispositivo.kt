a54815cf9ccdc4f30c27c400afb90e697bf59cda
Uneees <belabbes1021@gmail.com>
2024-05-31 12:44:52 +0200

    data class

--- app/src/main/java/com/example/devicetrack/data/model/Dispositivo.kt
+++ app/src/main/java/com/example/devicetrack/data/model/Dispositivo.kt
@@ -4,17 +4,21 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
-data class Dispositivos(
+data class Dispositivo(
     @SerializedName("Id_dispositivo") var Id_dispositivo: Int,
     @SerializedName("numero_serie") var numero_serie: String,
     @SerializedName("nombre") var nombre: String,
     @SerializedName("imagen") var imagen: String,
+    @SerializedName("favorito") var favorito: Int,
+    @SerializedName("conexion") var conexion: Int
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt() ?: 0,
         parcel.readString() ?: "",
         parcel.readString() ?: "",
         parcel.readString() ?: "",
+        parcel.readInt() ?: 0,
+        parcel.readInt() ?: 0,
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -22,18 +26,20 @@ data class Dispositivos(
         parcel.writeString(numero_serie)
         parcel.writeString(nombre)
         parcel.writeString(imagen)
+        parcel.writeInt(favorito)
+        parcel.writeInt(conexion)
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<Dispositivos> {
-        override fun createFromParcel(parcel: Parcel): Dispositivos {
-            return Dispositivos(parcel)
+    companion object CREATOR : Parcelable.Creator<Dispositivo> {
+        override fun createFromParcel(parcel: Parcel): Dispositivo {
+            return Dispositivo(parcel)
         }
 
-        override fun newArray(size: Int): Array<Dispositivos?> {
+        override fun newArray(size: Int): Array<Dispositivo?> {
             return arrayOfNulls(size)
         }
     }
--- app/src/main/java/com/example/devicetrack/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/devicetrack/ui/home/HomeViewModel.kt
@@ -1,37 +1,36 @@
 package com.example.devicetrack.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.devicetrack.data.model.Dispositivos
+import com.example.devicetrack.data.model.Dispositivo
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    val dispositivoModel = MutableLiveData<List<Dispositivos>?>()
-    val dispositivoFavModel = MutableLiveData<List<Dispositivos>?>()
+    val dispositivoModel = MutableLiveData<List<Dispositivo>?>()
+    val dispositivoFavModel = MutableLiveData<List<Dispositivo>?>()
     val isLoading = MutableLiveData<Boolean>()
 
     fun onCreate() {
         viewModelScope.launch {
             isLoading.postValue(true)
-            var result = emptyList<Dispositivos>()
+            var result = emptyList<Dispositivo>()
             result = listOf(
-                Dispositivos(1, "123456", "Dispositivo 1", "imagen1.jpg"),
-                Dispositivos(2, "789012", "Dispositivo 2", "imagen2.jpg"),
-                Dispositivos(3, "345678", "Dispositivo 3", "imagen3.jpg"))
+                Dispositivo(1, "123456", "Dispositivo 1", "imagen1.jpg", 1, 0),
+                Dispositivo(2, "789012", "Dispositivo 2", "imagen2.jpg", 0,1),
+                Dispositivo(3, "345678", "Dispositivo 3", "imagen3.jpg", 1, 1))
 
             if(!result.isNullOrEmpty()){
                 dispositivoModel.postValue(result)
                 isLoading.postValue(false)
             }
 
-            var result2 = emptyList<Dispositivos>()
+            var result2 = emptyList<Dispositivo>()
             result2 = listOf(
-                Dispositivos(1, "123456", "Favorito 1", "imagen1.jpg"),
-                Dispositivos(2, "789012", "Favorito 2", "imagen2.jpg"),
-                Dispositivos(3, "345678", "Favorito 3", "imagen3.jpg"))
+                Dispositivo(1, "123456", "Favorito 1", "imagen1.jpg",0,0),
+                Dispositivo(2, "789012", "Favorito 2", "imagen2.jpg",1,0),
+                Dispositivo(3, "345678", "Favorito 3", "imagen3.jpg",1,1))
 
                 dispositivoFavModel.postValue(result2)
         }
