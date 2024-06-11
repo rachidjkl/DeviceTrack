da797f4d32c8473f64fa371bb9f90b69fb2c7045
rachidjkl <rghenema2122@politecnics.barcelona>
2024-06-11 10:30:27 +0200

    dd

--- app/src/main/java/com/example/devicetrack/data/network/ApiClient.kt
+++ app/src/main/java/com/example/devicetrack/data/network/ApiClient.kt
@@ -3,6 +3,7 @@ package com.example.devicetrack.data.network
 import com.example.devicetrack.data.model.Dispositivo
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiClient {
     @GET("/dispositivos")
@@ -10,4 +11,7 @@ interface ApiClient {
 
     @GET("/dispositivos/favoritos/1")
     suspend fun getAllDispositivosFav(): Response<List<Dispositivo>>
+
+    @POST("/dispositivos/favoritos")
+    suspend fun PostAllDispositivosFav(): Response<List<Dispositivo>>
 }
