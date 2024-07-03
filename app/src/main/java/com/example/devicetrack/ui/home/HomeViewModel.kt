afbcad291508a12b9cb2fab0b921b7a300ba5a7b
rachidjkl <rghenema2122@politecnics.barcelona>
2024-07-03 13:36:08 +0200

    ss

--- app/src/main/java/com/example/devicetrack/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/devicetrack/ui/home/HomeViewModel.kt
@@ -30,11 +30,11 @@ class HomeViewModel : ViewModel() {
                 isLoading.postValue(false)
             }
             var result2 = emptyList<Dispositivo>()
-            //result2 = dispositivosRepo.getAllDispositivosFav()
+            result2 = dispositivosRepo.getAllDispositivosFav(idUser)
             dispositivoFavModel.postValue(result2)
 
 
-                dispositivoFavModel.postValue(result2)
+            dispositivoFavModel.postValue(result2)
 
         }
     }
